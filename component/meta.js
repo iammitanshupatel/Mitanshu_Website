3388bfea763d3caf472bb1b89f6c9b0e1ee80596
Mitanshu Patel <mitanshu.u.patel@gmail.com>
2021-07-23 14:42:39 +0530

    added meta tags

---
+++ component/meta.js
@@ -0,0 +1,47 @@
+export default function Meta() {
+  return (
+    <>
+      <meta name="application-name" content="Mitanshu Patel" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="Mitanshu Patel" />
+      <meta name="description" content="Mitanshu Patel Personal Portfolio" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-config" content="/browserconfig.xml" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      {/* <meta name="theme-color" content="#000000" /> */}
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      {/* <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+      /> */}
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="194x194" href="/favicon-194x194.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
+      {/* <link rel="manifest" href="/site.webmanifest" /> */}
+      {/* <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" /> */}
+      {/* <meta name="msapplication-TileColor" content="#00aba9" /> */}
+      {/* <meta name="msapplication-TileImage" content="/mstile-144x144.png" /> */}
+      {/* <meta name="theme-color" content="#ffffff" /> */}
+
+      {/* <link rel="shortcut icon" href="/favicon.ico" /> */}
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:url" content="https://mitanshu-website.vercel.app/" />
+      <meta name="twitter:title" content="Mitanshu Patel" />
+      <meta name="twitter:description" content="Mitanshu Patel personal portfolio" />
+      {/* <meta name="twitter:image" content="https://yagneshmodh.com/android-chrome-192x192.png" /> */}
+      <meta name="twitter:creator" content="@MitanshuPatel_" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Mitanshu Patel" />
+      <meta property="og:description" content="Mitanshu Patel personal portfolio" />
+      <meta property="og:site_name" content="Mitanshu Patel" />
+      <meta property="og:url" content="https://mitanshu-website.vercel.app/" />
+      {/* <meta property="og:image" content="https://yagneshmodh.com/apple-touch-icon.png" /> */}
+    </>
+  );
+}
--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Meta from '../component/meta';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <Meta />
+        </Head>
         <Main />
         <NextScript />
       </Html>
