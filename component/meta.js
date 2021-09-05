7489a58a300e622e7d1bbb39ea7e6c4e7accd5d5
Mitanshu Patel <mitanshu.u.patel@gmail.com>
2021-09-05 18:10:57 +0530

    updated meta tags

--- component/meta.js
+++ component/meta.js
@@ -1,45 +1,48 @@
 export default function Meta() {
   return (
     <>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0,viewport-fit=cover"  />
       <link rel="canonical" href="https://mitanshupatel.com/" />
+      <link rel="alternate" href="https://www.mitanshupatel.com/" hreflang="en-US" />
       <meta name="application-name" content="Mitanshu Patel" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content="Mitanshu Patel" />
       <meta name="description" content="Mitanshu Patel Personal Website" />
-      <meta name="format-detection" content="telephone=no" />
-      <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="msapplication-config" content="/browserconfig.xml" />
-      <meta name="msapplication-TileColor" content="#fff" />
-      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="google" content="notranslate">
+      <meta name="is-http2" content="true">
       <meta name="theme-color" content="#000000" />
-
-      <link rel="apple-touch-icon" href="/touch-icon-iphone.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/touch-icon-ipad.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/touch-icon-iphone-retina.png" />
-      <link rel="apple-touch-icon" sizes="167x167" href="/touch-icon-ipad-retina.png" />
-
-      <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
-      <link rel="manifest" href="/manifest.json" />
-      <link rel="mask-icon" href="/static/icons/safari-pinned-tab.svg" color="#5bbad5" />
-      <link rel="shortcut icon" href="/manifest-icon-192.png" />
       <meta name="Description" content="Mitanshu Patel Personal Website" />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:url" content="https://mitanshupatel.com/" />
-      <meta name="twitter:title" content="Mitanshu Patel" />
-      <meta name="twitter:description" content="Mitanshu Patel Personal Website" />
-      <meta name="twitter:image" content="https://mitanshupatel.com/manifest-icon-192.png" />
-      <meta name="twitter:creator" content="@MitanshuPatel_" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Mitanshu Patel" />
       <meta property="og:description" content="Mitanshu Patel Personal Website" />
       <meta property="og:site_name" content="Mitanshu Patel" />
       <meta property="og:url" content="https://mitanshupatel.com/" />
       <meta property="og:image" content="https://mitanshupatel.com/manifest-icon-192.png" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="Mitanshu Patel" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-config" content="/browserconfig.xml" />
+      <meta name="msapplication-TileColor" content="#fff" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <link rel="apple-touch-icon" href="/touch-icon-iphone.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/touch-icon-ipad.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/touch-icon-iphone-retina.png" />
+      <link rel="apple-touch-icon" sizes="167x167" href="/touch-icon-ipad-retina.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="mask-icon" href="/static/icons/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="shortcut icon" href="/manifest-icon-192.png" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:url" content="https://mitanshupatel.com/" />
+      <meta name="twitter:title" content="Mitanshu Patel" />
+      <meta name="twitter:description" content="Mitanshu Patel Personal Website" />
+      <meta name="twitter:image" content="https://mitanshupatel.com/manifest-icon-192.png" />
+      <meta name="twitter:creator" content="@MitanshuPatel_" />
       <meta name="robot" content="nonoindex,follow" />
       {/* apple splash screen images  */}
-
       <link rel="apple-touch-startup-image" href="/apple-splash-2048-2732.png" sizes="2048x2732" />
       <link rel="apple-touch-startup-image" href="/apple-splash-1668-2224.png" sizes="1668x2224" />
       <link rel="apple-touch-startup-image" href="/apple-splash-1536-2048.png" sizes="1536x2048" />
