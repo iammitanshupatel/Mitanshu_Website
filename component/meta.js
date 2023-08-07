4091c35e463fc9a5a1d3d858f5ed234930ec1f96
Mitanshu Patel <mitanshu.u.patel@gmail.com>
2023-08-08 00:28:49 +0530

    commit for editing meta tags

--- component/meta.js
+++ component/meta.js
@@ -3,59 +3,39 @@ export default function Meta() {
     <>
       <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0,viewport-fit=cover" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="language" content="EN" />
-      <link rel="canonical" href="https://mitanshupatel.com/" />
-      <meta name="application-name" content="Mitanshu Patel" />
-      <meta name="title" content="Mitanshu Patel" />
       <meta
         name="description"
-        content="Passionate Software Engineer | Full-stack Development | React, Node, React Native | Blockchain Solutions | Agile Methodology | CMMI Level 3 | Certified in CSS, JavaScript, React, and Rest API"
+        content="Passionate Software Engineer specializing in full-stack development using React, Node, and React Native. Certified in CSS, JavaScript, React, and Rest API."
       />
-      <meta name="google" content="notranslate" />
-      <meta name="is-http2" content="true" />
-      <meta name="theme-color" content="#000000" />
       <meta
         name="keywords"
-        content="mitanshu patel, personal website, portfolio, full-stack developer, front-end developer, mobile-app developer, blog, work, projects, education, skills, React.js, React Native, Redux.js, Next.js, Babel.js, Node.js, MySQL, Strapi.js, Advanced ES6 JavaScript, TypeScript, HTML5, CSS3, SASS, Tailwind CSS, Git, Github, Gitlab, BitBucket,  Agile Development, Attention to Detail, Responsive Web Design, Cross-browser Compatibility, Passionate Software Engineer, Full-stack Development, React, Node, React Native, Blockchain Solutions, Agile Methodology, CMMI Level 3, Certified in CSS, JavaScript, React, and Rest API"
+        content="mitanshu patel, personal portfolio, full-stack developer, React, Node, React Native, Agile Development, Blockchain Solutions"
       />
-      <meta name="author" content="Mitanshu Patel, mitanshu.u.patel@gmail.com" />
-      <meta name="reply-to" content="mitanshu.u.patel@gmail.com" />
-      <meta name="url" content="https://mitanshupatel.com/" />
-      <meta name="identifier-URL" content="https://mitanshupatel.com/" />
+      <meta name="author" content="Mitanshu Patel" />
+      <link rel="canonical" href="https://mitanshupatel.com/" />
       <meta property="og:title" content="Mitanshu Patel" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://mitanshupatel.com/" />
-      <meta property="og:site_name" content="Mitanshu Patel" />
-      <meta property="og:description" content="Mitanshu Patel Personal Website" />
+      <meta
+        property="og:description"
+        content="Passionate Software Engineer specializing in full-stack development using React, Node, and React Native. Certified in CSS, JavaScript, React, and Rest API."
+      />
       <meta property="og:image" content="https://mitanshupatel.com/manifest-icon-192.png" />
-      <meta name="og:email" content="mitanshu.u.patel@gmail.com" />
-      <meta name="og:phone_number" content="+91 91045 52342" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta content="yes" name="apple-touch-fullscreen" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content="Mitanshu Patel" />
-      <meta name="format-detection" content="telephone=no" />
-      <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="msapplication-config" content="/browserconfig.xml" />
-      <meta name="msapplication-TileColor" content="#fff" />
-      <meta name="msapplication-tap-highlight" content="no" />
-      <link rel="apple-touch-icon" target="_blank" href="/touch-icon-iphone.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/touch-icon-ipad.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/touch-icon-iphone-retina.png" />
-      <link rel="apple-touch-icon" sizes="167x167" href="/touch-icon-ipad-retina.png" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@MitanshuPatel_" />
+      <meta name="twitter:title" content="Mitanshu Patel" />
+      <meta
+        name="twitter:description"
+        content="Passionate Software Engineer specializing in full-stack development using React, Node, and React Native. Certified in CSS, JavaScript, React, and Rest API."
+      />
+      <meta name="twitter:image" content="https://mitanshupatel.com/manifest-icon-192.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
-      <link rel="manifest" target="_blank" href="/manifest.json" />
-      <link rel="mask-icon" href="/static/icons/safari-pinned-tab.svg" color="#5bbad5" />
-      <link rel="shortcut icon" href="/manifest-icon-192.png" />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:url" content="https://mitanshupatel.com/" />
-      <meta name="twitter:title" content="Mitanshu Patel" />
-      <meta name="twitter:description" content="Mitanshu Patel Personal Website" />
-      <meta name="twitter:image" content="https://mitanshupatel.com/manifest-icon-192.png" />
-      <meta name="twitter:creator" content="@MitanshuPatel_" />
-      {/* apple splash screen images  */}
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/manifest-icon-192.png" />
+    {/* apple splash screen images  */}
       <link rel="apple-touch-startup-image" href="/apple-splash-2048-2732.png" sizes="2048x2732" />
       <link rel="apple-touch-startup-image" href="/apple-splash-1668-2224.png" sizes="1668x2224" />
       <link rel="apple-touch-startup-image" href="/apple-splash-1536-2048.png" sizes="1536x2048" />
@@ -79,6 +59,5 @@ export default function Meta() {
         href="/static/images/apple-splash-640-1336.png"
         sizes="640x1136"
       />
-    </>
   );
 }
