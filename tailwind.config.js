fb49997c788c8f666ab8b4dff3de92845593cb14
Mitanshu Patel <mitanshu.u.patel@gmail.com>
2024-03-25 17:17:16 +0530

    removed unnecessary package

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,5 +64,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/line-clamp')],
+  plugins: [require('@tailwindcss/aspect-ratio')],
 };
